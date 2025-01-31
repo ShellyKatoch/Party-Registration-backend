41448239224bea70128af280d6f4391ee3f338f6
Shelly Katoch <shelly.22932+38@lpu.co.in>
2025-01-31 11:06:38 +0530

    fixes

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
+require('dotenv').config();
 const express = require("express");
 const router = require("./routes/payments.routes");
 const app = express();
-const port = 3000;
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
 const CourseRouter  = require("./routes/courseRoutes")
 
+
+const PORT = process.env.PORT || 3000
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/Coursedata',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error(err)) 
 
@@ -26,7 +25,7 @@ app.use('/uploads', express.static('uploads'));
 app.use('/api',router);
 app.use('/api',CourseRouter)
 
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
   });
   
